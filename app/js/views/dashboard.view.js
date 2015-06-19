07045406ec7ab5f8beda5f5ebb1bc540bcb8fdc6
robert fine <robertfine4631@icloud.com>
2015-06-19 11:06:09 -0400

    added initialize and render and IIFE functions to dashboard view

--- app/js/views/dashboard.view.js
+++ app/js/views/dashboard.view.js
@@ -2,4 +2,22 @@
 
 'use strict';
 
+app.views.dashboard = Backbone.Views.Extend({
+
+	className: '.dashboard',
+
+	template: hbs.dashboard,
+
+	initialize: function () {
+
+
+	},
+
+	render: function () {
+
+
+	}
+
 });
+
+}());
